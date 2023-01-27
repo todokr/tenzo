abd7c3c96a1a1a4aa9df916b9d990e71d371463a
todokr <s.tadokoro0317@gmail.com>
2023-01-27 18:41:01 +0900

    postgres

---
+++ tenzo-experience/postgres/init.sql
@@ -0,0 +1,19 @@
+create database example;
+
+create table departments (
+    id integer primary key,
+    department_name text not null,
+    parent_department_id integer references departments(id) null
+);
+
+create table users (
+    id integer primary key,
+    email text not null,
+    age integer not null,
+    department_id integer references departments(id) not null
+);
+
+create table target_users (
+    id integer primary key,
+    user_id integer references users(id) not null
+);
---
+++ tenzo-experience/src/main/scala/tenzo/JdbcConfig.scala
@@ -0,0 +1,3 @@
+package tenzo object JdbcConfig {
+
+}
