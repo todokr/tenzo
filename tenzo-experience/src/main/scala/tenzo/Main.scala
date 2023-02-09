e09bb6c44e1424922fd8ab27475a8e8a7659117f
todokr <s.tadokoro0317@gmail.com>
2023-02-09 23:23:26 +0900

    fix main

--- tenzo-experience/src/main/scala/tenzo/Main.scala
+++ tenzo-experience/src/main/scala/tenzo/Main.scala
@@ -41,27 +41,11 @@ object JDBCTest {
     val loader = new MetadataLoader(conf)
 
     val references = loader.loadReferences()
-    references.foreach(println)
+    val deps = references.dependencies("target_users")
+    println(deps)
 
-    val from = references.find(_.fromTable == "target_users")
-    val linear = Iterator
-      .iterate(from)(_.flatMap(r => references.find(_.fromTable == r.toTable)))
-      .takeWhile(x => x.nonEmpty && x.exists(r => r.fromTable != r.toTable))
-      .collect { case Some(x) => x }
-      .toSeq
-    val res = linear.head.fromTable +: linear.map(_.toTable)
-    println(res)
+    val tables = loader.loadTables(deps.distinct)
 
-    val tables     = references.flatMap(r => Seq(r.toTable, r.fromTable)).distinct
-    val structures = loader.loadTableStructure(tables)
-    structures.foreach { structure =>
-      println("=" * 30)
-      println(s"${structure.tableSchema}.${structure.tableName}")
-      println("=" * 30)
-      structure.columns.foreach { col =>
-        println(s"""${if (col.isPk) "[PK] " else ""}${col.name} ${col.dataType} ${if (col.nullable) "null" else "not null"}""")
-      }
-      println()
-    }
+    println(tables.toString)
   }
 }
