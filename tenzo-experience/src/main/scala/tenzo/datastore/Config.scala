cbcc511eaffba4a8ae28cca0e450113e41c0cf8b
todokr <s.tadokoro0317@gmail.com>
2023-02-12 09:22:22 +0900

    configurable schema

--- tenzo-experience/src/main/scala/tenzo/datastore/Config.scala
+++ tenzo-experience/src/main/scala/tenzo/datastore/Config.scala
@@ -15,28 +15,35 @@ object ConfigLoader {
 
 private[datastore] object JdbcConfig {
   def from(properties: Properties): JdbcConfig = {
-    val driver   = properties.valueOf(DriverKey)
-    val url      = properties.valueOf(UrlKey)
-    val user     = properties.valueOf(UserKey)
-    val password = properties.valueOf(PasswordKey)
-    JdbcConfig(driver, url, user, password)
+    val driver   = properties.getString(DriverKey)
+    val url      = properties.getString(UrlKey)
+    val user     = properties.getString(UserKey)
+    val password = properties.getString(PasswordKey)
+    val schema =  properties.getStringOr(SchemaKey, DefaultSchema)
+    JdbcConfig(driver, url, user, password, schema)
   }
 
   private val DriverKey   = "db.driver"
   private val UrlKey      = "db.url"
   private val UserKey     = "db.user"
   private val PasswordKey = "db.password"
+  private val SchemaKey   = "db.schema"
+
+  private val DefaultSchema = "public"
 
   private class JdbcConfigException(keyName: String) extends Exception(s"$keyName is not set")
 
   private implicit class RichProperties(props: Properties) {
-    def valueOf(key: String): String = Option(props.get(key))
-      .map(_.toString)
-      .getOrElse(throw new JdbcConfigException(key))
+
+    def getString(key: String): String =
+      props.computeIfAbsent(key, _ => throw new JdbcConfigException(key)).toString
+
+    def getStringOr(key: String, default: String): String =
+      props.computeIfAbsent(key, _ => default).toString
   }
 }
 
-final case class JdbcConfig(driver: String, url: String, user: String, password: String) {
+final case class JdbcConfig(driver: String, url: String, user: String, password: String, schema: String) {
   override def toString: String =
     s"""
        |driver=$driver
